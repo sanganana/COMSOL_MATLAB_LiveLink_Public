6f611d26911f365a6629d489a42fa0bac6206be8
Pawankumar <60772430+sanganana@users.noreply.github.com>
2024-02-09 20:50:42 +0530

    Update extracting_data_at_arbitrary_points.m

--- extracting_data_at_arbitrary_points.m
+++ extracting_data_at_arbitrary_points.m
@@ -13,6 +13,7 @@ data_u = mpheval(model,'u');    %struct of u    %%not needed
 ud1 = data_u.d1;                                %%not needed
 
 interpolated_data = mphinterp(model,'u','coord',[1.0051E-4;1.8784E-4]);%0.0017080
+% rows are for different time instants
 %1.0051E-4;1.8784E-4 are x and y coordinates of probe point where
 %interpolation is done
 %'u' is the variable name % no need to do data_u.d1
